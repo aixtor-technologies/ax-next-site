e791d4f43e707c822547c19bf9791bc69d083c03
Rajdip Vekriya <rajdip.vekriya@aixtor.com>
2026-03-06 15:11:43 +0530

    update home page
    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
+"use client"
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const API_BASE_URL = process.env.NEXT_PUBLIC_WP_API_URL;
+  const [homePage, setHomePage] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+      const fetchHomePage = async () => {
+        try {
+          const res = await fetch(`${API_BASE_URL}/wp/v2/home-page`);
+  
+          if (!res.ok) throw new Error("Failed to fetch home page data");
+  
+          const data = await res.json();
+          setHomePage(data || []);
+        } catch (error) {
+          console.error("Home Page API Error:", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchHomePage();
+  }, []);
   return (
     <div>
       <h1>Welcome to Our Website 🚀</h1>
-      <p>
-        This is the home page built with Next.js. We are using WordPress as a
-        headless CMS for content management.
-      </p>
-
-      <section>
-        <h2>Our Services</h2>
-        <ul>
-          <li>Web Development</li>
-          <li>CMS Solutions</li>
-          <li>API Integration</li>
-        </ul>
-      </section>
     </div>
   );
 }
