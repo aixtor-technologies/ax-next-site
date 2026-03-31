46096eb441b7dbf2c6e5414d557d4361188564c3
Rajdip Vekriya <rajdip.vekriya@aixtor.com>
2026-03-31 11:14:59 +0530

    loading file updated
    loading file updated

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,8 @@
+import Image from "next/image";
 export default function Loading() {
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "60vh" }}>
-      <div className="spinner-border text-danger" role="status" style={{ width: "3rem", height: "3rem" }}>
-        <span className="visually-hidden">Loading...</span>
-      </div>
+    <div className={`loader`}>
+      <Image src="/assets/images/Main-Loader.webp" alt="Loading" height={470} width={500} />
     </div>
   );
 }
